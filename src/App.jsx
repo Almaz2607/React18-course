359846cdf90cbd284e9e39b673ee83554445d2d0
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2025-04-03 18:50:56 +0600

    feat: drawing the game field

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import './App.css';
 
+const SYMBOL_X = 'X';
+const SYMBOL_O = 'O';
+
 function App() {
-  return <div>Hello React</div>;
+  const cells = [
+    SYMBOL_O,
+    SYMBOL_X,
+    null,
+    SYMBOL_O,
+    null,
+    SYMBOL_O,
+    null,
+    SYMBOL_X,
+    null,
+  ];
+  const currentStep = SYMBOL_X;
+
+  const getSymbolClassName = symbol => {
+    if (symbol === SYMBOL_O) return 'symbol--o';
+    if (symbol === SYMBOL_X) return 'symbol--x';
+    return '';
+  };
+
+  const renderSymbol = symbol => {
+    return (
+      <span className={`symbol ${getSymbolClassName(symbol)}`}>{symbol}</span>
+    );
+  };
+
+  return (
+    <div className="game">
+      <div className="game-info">Step: {renderSymbol(currentStep)}</div>
+      <div className="game-field">
+        {cells.map((symbol, index) => {
+          return (
+            <button key={index} className="cell">
+              {symbol ? renderSymbol(symbol) : null}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
